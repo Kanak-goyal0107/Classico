4ffe491badc8ac5289e55b16482b26c4b5076ce1
Kanak <goyalkanak0107@gmail.com>
2026-03-02 23:04:03 +0530

    comment

--- lib/file1.dart
+++ lib/file1.dart
@@ -1,3 +1,34 @@
+Future<String> getName() {
+	return Future.delayed(Duration(seconds: 2), () {
+		return "AJ";
+	});
+}
+
 void main(){
-  print("Hello World");
+  //print("Hello World");
+  /*String a = stdin.readLineSync() ?? "Guest";
+  print(a);
+  String? a;
+  a ??= "Guest";
+  print(a);
+	print("1) Start");
+	Future.delayed(Duration(seconds: 2000),(){
+		print("2) After 2 sec");
+	});
+	print("3)");
+	print("Start");
+	getName()
+			.then((value) {
+		print("Name is $value");
+	})
+			.catchError((e) {
+		print("Error $e");
+	})
+			.whenComplete(() {
+		print("Completed");
+	});
+	print("End");*/
+	dynamic x = 5;
+	x = "Hello";
+	print(x.length);
 }
